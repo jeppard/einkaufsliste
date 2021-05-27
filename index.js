52e4a157f828c3212f53249e4261d33660a91489
jeppard <35837126+jeppard@users.noreply.github.com>
2021-05-27 17:54:53 +0200

    Added firstPlaceholder for Lists
    Added site for List

--- index.js
+++ index.js
@@ -8,13 +8,14 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.use('/static', express.static('app/pages/index.html'))
-app.use('/app/scripts/index.js', express.static('app/scripts/index.js'))
+app.use('/static', express.static('app/pages/'))
+app.use('/app/scripts/', express.static('app/scripts/'))
+app.use('/app/styles/', express.static('app/styles/'))
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
 
-app.use('/list', (req, res)=>{
+app.use('/list', (req, res) => {
     res.send('{"list": ["Ich bin das Erste Element", "Ich bin das zweite Element"]}')
 })
