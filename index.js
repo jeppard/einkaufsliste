29ba2642c62b9739a1cb1f0c9b4c3d927caf7121
TK221 <51761737+TK221@users.noreply.github.com>
2021-06-01 17:41:21 +0200

    Added init database enpoint, userID and description to article, lists route, improved comments, database calls, minor fixes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const db = require("./lib/database/db");
-require("./lib/database/test")
+const db = require('./lib/database/db');
+const test = require('./lib/database/test');
+const lists = require('./lib/routes/lists');
 
 const app = express();
 const port = 3000;
@@ -11,6 +12,7 @@ app.get('/', (req, res) => {
 
 app.use('/static', express.static('app/pages/index.html'))
 app.use('/app/scripts/index.js', express.static('app/scripts/index.js'))
+app.use('/lists', lists);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
@@ -19,3 +21,7 @@ app.listen(port, () => {
 app.use('/list', (req, res) => {
     res.send('{"list": ["Ich bin das Erste Element", "Ich bin das zweite Element"]}')
 })
+
+app.get('/init', async function (req, res) {
+    test.initDatabase();
+});
