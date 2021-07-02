8de45326d9219073a92ad2807d89f3421c75d23a
TK221 <51761737+TK221@users.noreply.github.com>
2021-07-02 13:19:14 +0200

    Added article type object to article object and updated all dependent functions close #30

--- lib/database/test.ts
+++ lib/database/test.ts
@@ -4,7 +4,6 @@ import * as listProvider from './provider/list';
 import * as articleProvider from './provider/articel';
 import * as elementProvider from './provider/element';
 import * as articleTypeProvider from './provider/article_type';
-import { Article } from './types/article';
 
 export async function initDatabase (): Promise<void> {
     await accountProvider.initDatabase();
@@ -23,8 +22,8 @@ export async function initData (): Promise<void> {
     await articleTypeProvider.addType('Werkzeug', '#6C8188');
     await articleTypeProvider.addType('Material', '#782A1C');
 
-    await articleProvider.addArticle(new Article(1, 1, 'Apfel', 'Ein netter Apfel', 1));
-    await articleProvider.addArticle(new Article(2, 1, 'Baum', 'Ein Baum', 3));
+    await articleProvider.addArticle(1, 'Apfel', 'Ein netter Apfel', 1);
+    await articleProvider.addArticle(1, 'Baum', 'Ein Baum', 3);
 
     await elementProvider.addElement(1, 1, 10, 'Stück');
     await elementProvider.addElement(1, 2, 10, 'Bäume');
