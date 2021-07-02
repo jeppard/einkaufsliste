bc0984e15e8eae2f7e081360315f6fd9e4389b00
jeppard <35837126+jeppard@users.noreply.github.com>
2021-07-02 10:38:36 +0200

    temp commit

--- app/scripts/addArticle.js
+++ app/scripts/addArticle.js
@@ -1,26 +1,29 @@
 let searchParams = new URLSearchParams(window.location.search)
 let articleID = searchParams.get('articleId');
 let listID = searchParams.get('listID');
-
 let type = 1; //TODO select Type
 
-if (articleID != null) {
-    fetch(window.location.origin + "/lists/articles/get", {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                "articleID": articleID
+
+function init() {
+    if (articleID != null) {
+        fetch(window.location.origin + "/lists/articles/get", {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    "articleID": articleID
+                })
+            }).then(data => data.json())
+            .then(data => {
+                document.getElementById("description").value = data.description;
+                document.getElementById("article_name").value = data.name;
+                type = data.type;
+                userID = data.userID;
             })
-        }).then(data => data.json())
-        .then(data => {
-            document.getElementById("description").value = data.description;
-            document.getElementById("article_name").value = data.name;
-            type = data.type;
-            userID = data.userID;
-        })
+    }
 }
+
 
 
 function submitFunction(params) {
