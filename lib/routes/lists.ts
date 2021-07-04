afd39ba598e70c9154420558fa2863b91d8c1ef7
TK221 <51761737+TK221@users.noreply.github.com>
2021-07-04 12:40:46 +0200

    Added add, get, getListsOfUsers and get UsersOfList endpoints to list route

--- lib/routes/lists.ts
+++ lib/routes/lists.ts
@@ -3,7 +3,8 @@ import { elementRouter } from './elements';
 import { articleRouter } from './articles';
 import * as elementProvider from '../database/provider/element';
 import * as listProvider from '../database/provider/list';
-import { areNumbers } from '../parameter_util';
+import * as linkUserListProvider from '../database/provider/link_user_list';
+import { areNumbers, areNotNullButEmpty, areNotNullOrEmpty } from '../parameter_util';
 
 const router = express.Router();
 
@@ -60,6 +61,108 @@ router.post('/removeList', async function (req, res) {
         }
     } else {
         res.status(400).send('Failed');
+    }
+});
+
+/**
+ * Add list to database
+ *
+ * route: "/lists/add"
+ *
+ * Body:
+ * name
+ * ownerID
+ * desription
+ *
+ * return:
+ * listID
+ */
+router.post('/add', async function (req, res) {
+    const body: { name: string, ownerID: number, desription: string } = req.body;
+
+    if (body && areNumbers([body.ownerID]) && areNotNullOrEmpty([body.name]) && areNotNullButEmpty([body.desription])) {
+        const listID = await listProvider.addList(body.name, body.ownerID, body.desription);
+
+        if (listID) {
+            res.status(200).send(listID);
+        } else res.status(500).send('Failed to add list');
+    } else {
+        res.status(400).send('Incorrect body');
+    }
+});
+
+/**
+ * get list from database
+ *
+ * route: "/lists/get"
+ *
+ * Body:
+ * listID
+ *
+ * return:
+ * list
+ */
+router.get('/get', async function (req, res) {
+    const body: { listID: number } = req.body;
+
+    if (body && areNumbers([body.listID])) {
+        const list = await listProvider.getListById(body.listID);
+
+        if (list) {
+            res.status(200).send(list);
+        } else res.status(404).send('Failed to get list');
+    } else {
+        res.status(400).send('Incorrect body');
+    }
+});
+
+/**
+ * get lists of user from database
+ *
+ * route: "/lists/getListsOfUser"
+ *
+ * Body:
+ * userID
+ *
+ * return:
+ * Array of lists
+ */
+router.get('/getListsOfUser', async function (req, res) {
+    const body: { userID: number } = req.body;
+
+    if (body && areNumbers([body.userID])) {
+        const lists = await linkUserListProvider.getListsByUser(body.userID);
+
+        if (lists) {
+            res.status(200).send(lists);
+        } else res.status(404).send('Failed to get list of user');
+    } else {
+        res.status(400).send('Incorrect body');
+    }
+});
+
+/**
+ * get users of list from database
+ *
+ * route: "/lists/getUsersOfList"
+ *
+ * Body:
+ * listID
+ *
+ * return:
+ * Array of Users
+ */
+router.get('/getUsersOfList', async function (req, res) {
+    const body: { listID: number } = req.body;
+
+    if (body && areNumbers([body.listID])) {
+        const users = await linkUserListProvider.getUsersByList(body.listID);
+
+        if (users) {
+            res.status(200).send(users);
+        } else res.status(404).send('Failed to get users of list');
+    } else {
+        res.status(400).send('Incorrect body');
     }
 });
 
