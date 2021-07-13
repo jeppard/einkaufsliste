5a6c8ef537fcab5771c0eaec2444081717d401f7
jeppard <35837126+jeppard@users.noreply.github.com>
2021-07-13 17:06:20 +0200

    Added Login function

---
+++ app/scripts/login.js
@@ -0,0 +1,39 @@
+function signin() {
+    const usernameField = document.getElementById("username");
+    const passwordField = document.getElementById("password");
+    const errorField = document.getElementById("upWarn");
+    let valid = true;
+    if (usernameField.value == "") {
+        valid = false;
+        usernameField.style.backgroundColor = "red";
+        usernameField.onfocus = () => {
+            usernameField.style.backgroundColor = "";
+        }
+    }
+    if (passwordField.value == "") {
+        valid = false;
+        passwordField.style.backgroundColor = "red";
+        passwordField.onfocus = () => {
+            passwordField.style.backgroundColor = "";
+        }
+    }
+    if (valid) {
+        fetch(window.location.origin + "/auth/signin", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    "username": usernameField.value,
+                    "password": passwordField.value,
+                })
+            })
+            .then(response => {
+                if (response.ok) {
+                    window.location.assign(window.location.origin + "/static/dashbord.html");
+                } else {
+                    errorField.style.visibility = "";
+                }
+            })
+    }
+}
