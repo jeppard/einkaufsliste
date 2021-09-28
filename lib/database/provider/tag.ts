26dd788eee4067925ed7205eeb889fe0f739fd3c
jeppard <35837126+jeppard@users.noreply.github.com>
2021-09-28 17:50:16 +0200

    added getTagID
    Get Tag by Name and List ID

--- lib/database/provider/tag.ts
+++ lib/database/provider/tag.ts
@@ -86,3 +86,18 @@ export async function removeTagIfUnused (tagID: number): Promise<void> {
     if (!(await checkTagArticleUse(tagID) && (await checkTagElementUse(tagID)))) return;
     removeTag(tagID);
 }
+
+export async function getTagID (tagName: string, listID: number): Promise<number> {
+    let conn;
+    let res = -1;
+    try {
+        conn = await getConnection();
+        const rows = await conn.query('SELECT id FROM ' + TAG_TABLE_NAME + ' WHERE name=? AND listID=? LIMIT 1;', [tagName, listID]);
+        if (rows && rows.length > 0) res = rows[0];
+    } catch (error) {
+        console.log('Could not find tag "' + tagName + '" in list with id ' + listID + ': ' + error);
+    } finally {
+        if (conn) conn.end();
+    }
+    return res;
+}
